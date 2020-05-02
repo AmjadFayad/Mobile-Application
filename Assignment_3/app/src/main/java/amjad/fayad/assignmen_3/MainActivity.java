96b543347cc268b9598debe7ef6aff8841d208b5
Rodrigo Morales <rodrigomoralesdev@gmail.com>
2020-05-02 14:45:49 -0600

    Hidding and showing table

--- Assignment_3/app/src/main/java/amjad/fayad/assignmen_3/MainActivity.java
+++ Assignment_3/app/src/main/java/amjad/fayad/assignmen_3/MainActivity.java
@@ -3,6 +3,11 @@ package amjad.fayad.assignmen_3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText editText95 = findViewById(R.id.edOctan95);
+        final EditText editText98 = findViewById(R.id.edOctan98);
+
+        final TextView textView95 = findViewById(R.id.octan95Val);
+        final TextView textView98 = findViewById(R.id.octan98Val);
+
+        Button btnShow = findViewById(R.id.btnShow);
+        final TableLayout tableLayout = findViewById(R.id.table);
+
+        btnShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tableLayout.setVisibility(View.VISIBLE);
+
+                textView95.setText(editText95.getText());
+                textView98.setText(editText98.getText());
+            }
+        });
     }
 }
