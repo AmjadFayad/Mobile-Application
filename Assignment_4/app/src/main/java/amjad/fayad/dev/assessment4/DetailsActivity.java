5a02882f915b6de1063df1cb1b1c9a036734c1c0
Rodrigo Morales <rodrigomoralesdev@gmail.com>
2020-06-07 14:35:24 -0600

    Details activity ready

--- Assignment_4/app/src/main/java/amjad/fayad/dev/assessment4/DetailsActivity.java
+++ Assignment_4/app/src/main/java/amjad/fayad/dev/assessment4/DetailsActivity.java
@@ -8,8 +8,22 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class DetailsActivity extends AppCompatActivity {
+
+    private String currencyName;
+    private Integer bdlVal;
+    private Integer realVal;
+    private Integer image;
+
+    private Integer resultBdl;
+    private Integer resultRealVal;
+    private Integer resultGap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +33,22 @@ public class DetailsActivity extends AppCompatActivity {
         // Sets back btn to navbar
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        // Get Intent data
+        getData();
+
+        TextView tvCurrencyName = findViewById(R.id.tv_currency_details);
+        tvCurrencyName.setText(currencyName);
+
+        ImageView currencyImage = findViewById(R.id.iv_currency_details);
+        Picasso.get().load(image).into(currencyImage);
+
         EditText currencyInput = findViewById(R.id.et_currency_input);
         Button btnCalculate = findViewById(R.id.btn_calculate);
+
+        LinearLayout resultsLayout = findViewById(R.id.ll_results);
+        TextView tvBdl = findViewById(R.id.tv_vbdl_details);
+        TextView tvRealVal = findViewById(R.id.tv_real_details);
+        TextView tvGap = findViewById(R.id.tv_gap_details);
 
         btnCalculate.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,20 +56,44 @@ public class DetailsActivity extends AppCompatActivity {
                 Integer rawInput = Integer.parseInt(currencyInput.getText().toString());
 
                 if (rawInput > 0) {
-
+                    computeResults(rawInput);
+                    resultsLayout.setVisibility(View.VISIBLE);
+                    setResults(tvBdl, tvRealVal, tvGap);
                 }
             }
         });
     }
 
     /**
-     * Computes results (BDL, Real Value and Gap) for the respective user input
-     * @param vbdl
-     * @param realvalue
-     * @param amount
+     * Obtains intent data
      */
-    private void computeResults(Integer vbdl, Integer realvalue, Integer amount) {
+    private void getData() {
+        currencyName = getIntent().getStringExtra("name");
+        bdlVal = getIntent().getIntExtra("vbdl", 0);
+        realVal = getIntent().getIntExtra("real", 0);
+        image = getIntent().getIntExtra("image", R.drawable.coin);
+    }
 
+    /**
+     * Computes results (BDL, Real Value and Gap) for the respective user input
+     * @param input user input in Lebanese pound
+     */
+    private void computeResults(Integer input) {
+        resultBdl = input/bdlVal;
+        resultRealVal = input/realVal;
+        resultGap = resultRealVal - resultBdl;
+    }
+
+    /**
+     * Sets values obtained from computeResults() to the xml widgets
+     * @param bdl tv
+     * @param realVal tv
+     * @param gap tv
+     */
+    private void setResults(TextView bdl, TextView realVal, TextView gap) {
+        bdl.setText(getResources().getString(R.string.price_according_to_bdl) + " " + resultBdl);
+        realVal.setText(getResources().getString(R.string.real_price) + " " + resultRealVal);
+        gap.setText(getResources().getString(R.string.gap) + " " + resultGap);
     }
 
     @Override
