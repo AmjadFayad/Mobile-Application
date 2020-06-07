51e4f99649971e5326af2830669754e0765e94e5
Rodrigo Morales <rodrigomoralesdev@gmail.com>
2020-06-06 18:13:40 -0600

    java stuff to compute results

--- Assignment_4/app/src/main/java/amjad/fayad/dev/assessment4/DetailsActivity.java
+++ Assignment_4/app/src/main/java/amjad/fayad/dev/assessment4/DetailsActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class DetailsActivity extends AppCompatActivity {
 
@@ -13,7 +16,32 @@ public class DetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
+        // Sets back btn to navbar
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        EditText currencyInput = findViewById(R.id.et_currency_input);
+        Button btnCalculate = findViewById(R.id.btn_calculate);
+
+        btnCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer rawInput = Integer.parseInt(currencyInput.getText().toString());
+
+                if (rawInput > 0) {
+
+                }
+            }
+        });
+    }
+
+    /**
+     * Computes results (BDL, Real Value and Gap) for the respective user input
+     * @param vbdl
+     * @param realvalue
+     * @param amount
+     */
+    private void computeResults(Integer vbdl, Integer realvalue, Integer amount) {
+
     }
 
     @Override
